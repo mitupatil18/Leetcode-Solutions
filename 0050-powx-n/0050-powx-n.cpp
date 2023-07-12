ebf46a24bb4d54d0b7ce70dad3c57b8148779cb8
Mitali Patil <117500694+mitupatil18@users.noreply.github.com>
2023-07-12 08:32:31 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (93.77%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,19 @@
+#include<cmath>
+class Solution {
+public:
+    double myPow(double x , int n) {
+        if(n==0)
+        return double(1);
+        double ans=myPow(x,n/2);
+        if(n%2==0)
+        ans=ans*ans;
+        else
+        {
+            if(n>0)
+            ans=(x*ans*ans);
+            else
+            ans=(ans*ans)/x;
+        }
+        return ans;
+    }
+};
