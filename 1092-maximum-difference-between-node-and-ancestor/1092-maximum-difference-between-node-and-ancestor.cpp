1cf736e06385c3ac15ce34aa5ca91a158ca5343a
Mitali Patil <117500694+mitupatil18@users.noreply.github.com>
2023-07-29 10:42:09 +0530

    Time: 0 ms (100%), Space: 9.6 MB (88.47%) - LeetHub

--- 1092-maximum-difference-between-node-and-ancestor/1092-maximum-difference-between-node-and-ancestor.cpp
+++ 1092-maximum-difference-between-node-and-ancestor/1092-maximum-difference-between-node-and-ancestor.cpp
@@ -11,21 +11,26 @@
  */
 class Solution {
 public:
-int ans = INT_MIN;
-    void find(TreeNode *a, vector<int>& v )
-    {
-        if(!a) return ;
-        for(auto x:v)
-        ans=max(ans,abs(a->val-x));
-        v.push_back(a->val);
-        find(a->left,v);
-        find(a->right,v);
-        v.pop_back();
+    int ans=0;
+    void solve(TreeNode* root,int mini,int maxi){
+        if(!root->left && !root->right){
+            mini = min(mini,root->val);
+            maxi = max(maxi,root->val);
+            ans = max(ans,abs(mini-maxi));
+            return;
+        }
+        
+        if(root->left){
+            ans = max(ans,abs(mini-maxi));
+            solve(root->left,min(mini,root->left->val),max(maxi,root->left->val));
+        }
+        if(root->right){
+            ans = max(ans,abs(mini-maxi));
+            solve(root->right,min(mini,root->right->val),max(maxi,root->right->val));
+        }
     }
-    int maxAncestorDiff(TreeNode* a) {
-        if(!a) return INT_MIN ;
-        vector<int> v ;
-        find(a,v);
-        return ans ;
+    int maxAncestorDiff(TreeNode* root) {
+        solve(root,root->val,root->val);
+        return ans;
     }
 };
