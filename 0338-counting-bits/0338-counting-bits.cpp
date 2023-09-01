0b7b73bf89e106ebf5e86630948980fd4d75d91c
Mitali Patil <117500694+mitupatil18@users.noreply.github.com>
2023-09-01 06:39:25 +0530

    Time: 6 ms (56.68%), Space: 7.8 MB (65.8%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> ans(n+1,0);
-        if(n==0) return ans ;
-        for(int i = 1 ; i<=n ; i++)
+        vector<int> v(n+1,0);
+        for(int i = 0 ; i<n+1 ; i++)
         {
-            if(i%2==0)
-                ans[i] = ans[i/2] ;
-            else 
-                ans[i] = ans[i/2]+1 ;
+            if((i)%2==0)
+            v[i] = v[(i)/2];
+            else
+            v[i] = v[(i)/2]+1 ;
         }
-        return ans ;
+        return v ;
     }
 };
