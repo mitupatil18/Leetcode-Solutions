a0ca2304c12a174c379aecb60e95d8bf0e6999c2
Mitali Patil <mitupatil39@gmail.com>
2023-09-29 16:51:24 +0530

    Time: 4 ms (73.86%), Space: 13.4 MB (34.84%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int ans = 0 , i , n = prices.size();
-        for(i = 0 ; i<n-1 ; i++)
-        {
-            if(prices[i]<prices[i+1])
-            {
-                ans += prices[i+1]-prices[i] ;
-            }
+        int count=0;
+        int n = prices.size();
+        for(int i=0;i<n-1;i++){
+            int temp=0;
+            if(prices[i]<prices[i+1]) temp=prices[i+1]-prices[i];
+            count=count+temp;
         }
-        return ans ;
+        return count;
     }
 };
