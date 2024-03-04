707036bab00d43e58a07c3d5667b6af98d7665d5
Mitali Patil <mitupatil39@gmail.com>
2024-03-04 07:26:37 +0530

    Time: 4 ms (74.87%) | Memory: 13.2 MB (23.53%) - LeetSync

---
+++ 985-bag-of-tokens/bag-of-tokens.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& t, int power) {
+        sort(t.begin(),t.end());
+        int i = 0 , j = t.size()-1 , s = 0 , ans = 0  ;
+        while(i<=j)
+        {
+            if( power>=t[i])
+            {
+                power -= t[i];
+                s++;
+                ans = max(ans,s);
+                i++;
+            }
+            else if(s>0 && power<t[j])
+            {
+                s--;
+                ans = max(ans,s);
+                power += t[j];
+                j--;
+            }
+            else
+            break;
+        }
+        return ans;
+    }
+};
