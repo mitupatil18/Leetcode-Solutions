8d281fa57a8ab1a1d64d15b1fef8014233efc54f
Mitali Patil <mitupatil39@gmail.com>
2024-03-21 11:34:05 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (84.88%) - LeetSync

---
+++ 72-edit-distance/edit-distance.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minDistance(string s, string t) {
+        int n = s.size(), m =t.size();
+        int dp[n+1][m+1];
+        for(int i = 0 ; i<=n ;i++)
+        {
+            for(int j = 0 ; j<=m ;j++)
+            {
+                if(i==0 || j==0)
+                dp[i][j] = max(i,j);
+                else if(s[i-1]==t[j-1])
+                dp[i][j] = dp[i-1][j-1];
+                else
+                dp[i][j] = 1+min(dp[i][j-1], min(dp[i-1][j],dp[i-1][j-1]));
+            }
+        }
+        return dp[n][m];
+    }
+};
