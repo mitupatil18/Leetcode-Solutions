376245707bcf24a2a3f4521a54e47593e11082f6
Mitali Patil <mitupatil39@gmail.com>
2024-03-25 08:57:19 +0530

    Time: 231 ms (5.27%) | Memory: 46.6 MB (31.54%) - LeetSync

---
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        priority_queue<pair<int,int>> q;
+        set<int> sett;
+        int n = arr.size();
+        for(int i = 0 ; i<arr.size() ;i++)
+        {
+            sett.insert(arr[i]);
+            q.push({arr[i],i});
+        }
+        int f = 1, prev;
+        n = sett.size();
+        while(!q.empty())
+        {
+            auto x = q.top();
+            q.pop();
+            cout<<x.first<<" "<<x.second<<endl;
+            if(f!=1 && x.first!=prev)
+            {
+                n--;
+            }
+            else 
+            f= 0 ;
+            arr[x.second] = n;
+            prev = x.first ;
+        }
+        return arr;
+    }
+};
