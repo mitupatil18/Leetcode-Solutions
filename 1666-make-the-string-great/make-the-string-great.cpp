8d856c2ca8b0c0b43f5cb9bc99a2567fd72b0a63
Mitali Patil <mitupatil39@gmail.com>
2024-04-01 18:26:26 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (25.44%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.cpp
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -2,17 +2,15 @@ class Solution {
 public:
     string makeGood(string s) {
         stack<char> st;
+        string z;
         st.push(s[0]);
         for(int i = 1 ; i<s.size() ;i++)
         {
             if(!st.empty() && abs(st.top()-s[i])==32 )
-            {
-                st.pop();
-            }
+            st.pop();
             else
             st.push(s[i]);
         }
-        string z;
         while(!st.empty())
         {
             z.push_back(st.top());
