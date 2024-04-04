13083d8e9910a81218426336a6d77fb5304f7103
Mitali Patil <mitupatil39@gmail.com>
2024-04-04 15:32:54 +0530

    Time: 244 ms (51.78%) | Memory: 162.7 MB (39.19%) - LeetSync

--- 2567-closest-nodes-queries-in-a-binary-search-tree/closest-nodes-queries-in-a-binary-search-tree.cpp
+++ 2567-closest-nodes-queries-in-a-binary-search-tree/closest-nodes-queries-in-a-binary-search-tree.cpp
@@ -1,41 +1,36 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void find(TreeNode *a , vector<int>& v)
-    {
-        if(a==NULL)
-        return ;
-        find(a->left , v);
+    void find(TreeNode* a, vector<int>& v) {
+        if (a == NULL)
+            return;
+        find(a->left, v);
         v.push_back(a->val);
-        find(a->right , v);
+        find(a->right, v);
     }
-    vector<vector<int>> closestNodes(TreeNode* root, vector<int>& queries) {
-        vector<int> arr;
-        int n = queries.size();
-        vector<vector<int>> ans ;
-        find(root,arr);
-        
-        for(int i = 0 ; i<queries.size();i++)
-        {
-            int ind=lower_bound(begin(arr),end(arr),queries[i])-begin(arr);
-            vector<int>ansq(2,0);
-            ansq[1]=(ind==arr.size())?-1:arr[ind]; 
-            if(ind<arr.size() and arr[ind]==queries[i]) ansq[0]=arr[ind];  
-            else if(ind == 0) ansq[0]=-1;
-            else  ansq[0]=arr[--ind];
-            ans.push_back(ansq);
+    
+    vector<vector<int>> closestNodes(TreeNode* root, vector<int>& q) {
+        vector<vector<int>> ans;
+        vector<int> v;
+        find(root, v);
+        for (int i = 0; i < q.size(); i++) {
+            int l = 0, r = v.size() - 1, mid;
+            while (l <= r) { 
+                mid = l + (r - l) / 2;
+                if (v[mid] == q[i]) {
+                    ans.push_back({q[i], q[i]}); 
+                    break;
+                } else if (v[mid] > q[i]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            }
+            if (ans.size() == i) {
+                int la = (r >= 0) ? v[r] : -1;
+                int lb = (l < v.size()) ? v[l] : -1;
+                ans.push_back({la, lb});
+            }
         }
-
-        return ans ;
+        return ans;
     }
 };
