315dbe789ee9ce3186431c1eb0e773e92ccf9407
Mitali Patil <mitupatil39@gmail.com>
2024-04-13 22:40:50 +0530

    Time: 239 ms (100.00%) | Memory: 118.2 MB (60.00%) - LeetSync

--- 3390-minimum-rectangles-to-cover-points/minimum-rectangles-to-cover-points.cpp
+++ 3390-minimum-rectangles-to-cover-points/minimum-rectangles-to-cover-points.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int minRectanglesToCoverPoints(vector<vector<int>>& p, int w) {
-        vector<int> v ;
         int n = p.size() ;
         sort(p.begin(),p.end());
-        for(int i = 0 ; i<n ;i++)
-        v.push_back(p[i][0]);
         int j = 0, i = 0, cnt = 0 ;
         while(i<n)
         {
-            while(j<n && abs(v[j]-v[i])<=w)
+            while(j<n && abs(p[j][0]-p[i][0])<=w)
             j++ ;
             cnt++;
             i = j ;
