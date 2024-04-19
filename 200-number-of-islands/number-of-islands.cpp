398facb7df179cc7b333e40fcfa748c91bb9b25a
Mitali Patil <mitupatil39@gmail.com>
2024-04-19 06:35:45 +0530

    Time: 29 ms (55.47%) | Memory: 22.7 MB (27.35%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,45 +1,43 @@
 class Solution {
 public:
-    int numIslands(vector<vector<char>>& g) {
-        
-        int n = g.size() , m = g[0].size();
-        vector<vector<int>> v( n , vector<int> (m, 0)); 
-        int ans = 0 ;
-        for(int i = 0 ; i<n ;i++)
+    int numIslands(vector<vector<char>>& grid) {
+        int ans = 0 , r = grid.size() , c = grid[0].size();
+        vector<vector<int>> vis(r, vector<int>(c, 0));
+        for(int i = 0 ; i<r ;i++)
         {
-            for(int j = 0 ; j<m ;j++)
+            for(int j = 0 ; j<c ;j++)
             {
-                if(!v[i][j] && g[i][j]=='1')
+                if(grid[i][j]=='1' && !vis[i][j])
                 {
-                    v[i][j] = 1;
+                    vis[i][j] = 1;
+                    ans++;
                     queue<pair<int,int>> q;
                     q.push({i,j});
                     while(!q.empty())
                     {
-                        int x = q.front().first , y = q.front().second;
+                        int x = q.front().first, y = q.front().second;
                         q.pop();
-                        if(x+1<n && !v[x+1][y] && g[x+1][y]=='1')
+                        if(x+1<r && !vis[x+1][y] && grid[x+1][y]=='1')
                         {
-                            v[x+1][y] = 1;
+                            vis[x+1][y] = 1;
                             q.push({x+1,y});
                         }
-                        if(x-1>=0 && !v[x-1][y] && g[x-1][y]=='1')
+                        if(x-1>=0 && !vis[x-1][y] && grid[x-1][y]=='1')
                         {
-                            v[x-1][y] = 1;
+                            vis[x-1][y] = 1;
                             q.push({x-1,y});
                         }
-                        if(y-1>=0 && !v[x][y-1] && g[x][y-1]=='1')
+                        if(y-1>=0 && !vis[x][y-1] && grid[x][y-1]=='1')
                         {
-                            v[x][y-1] = 1;
+                            vis[x][y-1] = 1;
                             q.push({x,y-1});
                         }
-                        if(y+1<m && !v[x][y+1] && g[x][y+1]=='1')
+                        if(y+1<c && !vis[x][y+1] && grid[x][y+1]=='1')
                         {
-                            v[x][y+1] = 1;
+                            vis[x][y+1] = 1;
                             q.push({x,y+1});
                         }
                     }
-                    ans++;
                 }
             }
         }
