c8720adf52d3422ff1b3570317d4394ae5ac5373
Mitali Patil <mitupatil39@gmail.com>
2024-04-23 06:44:10 +0530

    Time: 7 ms (83.25%) | Memory: 18.4 MB (72.51%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,44 +1,43 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int maj1 = 0, maj2 = 0, cnt1 = 0 , cnt2 = 0 ;
+        int cnt1 = 0 , cnt2 = 0;
+        int maj1 = 0 , maj2 = 0 ;
         for(int i = 0 ; i<nums.size() ;i++)
         {
             if(nums[i]==maj1)
-            cnt1++;
+            cnt1++ ;
             else if(nums[i]==maj2)
             cnt2++;
             else if(cnt1==0)
             {
-                maj1 = nums[i];
+                maj1= nums[i];
                 cnt1++;
             }
             else if(cnt2==0)
             {
-                maj2 = nums[i];
+                maj2= nums[i];
                 cnt2++;
             }
             else
             {
-                cnt1-- ;
+                cnt1--;
                 cnt2--;
             }
         }
-        cnt1= 0 , cnt2 = 0;
-        for(int i = 0 ; i<nums.size();i++)
+        cnt1 = 0 , cnt2 = 0 ;
+        for(int i = 0 ; i<nums.size() ;i++)
         {
-            if(nums[i]==maj1) cnt1++;
-            else if(nums[i]==maj2) cnt2++;
+            if(nums[i]==maj1)
+            cnt1++ ;
+            else if(nums[i]==maj2)
+            cnt2++ ;
         }
         vector<int> res;
-
-        if (cnt1 > nums.size() / 3) {
-            res.push_back(maj1);
-        }
-        if (cnt2 > nums.size() / 3) {
-            res.push_back(maj2);
-        }
-
-        return res;  
+        if(cnt1>nums.size()/3)
+        res.push_back(maj1);
+        if(cnt2>nums.size()/3)
+        res.push_back(maj2);
+        return res;
     }
 };
