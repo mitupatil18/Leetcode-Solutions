8466595004d01794854eda6eee3f25bbe6bb084f
Mitali Patil <mitupatil39@gmail.com>
2024-05-24 13:56:30 +0530

    Time: 7 ms (98.61%) | Memory: 21.5 MB (73.28%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int start=0,n=nums.size(); 
-        for(int i=0;i<nums.size();i++)
-        {  
-            if(nums[i]!=0) 
-            nums[start++]=nums[i];
+        int l = 0, n = nums.size();
+        for(int r = 0 ; r<n ;r++)
+        {
+            if(nums[r]!=0)
+            {
+                swap(nums[l],nums[r]);
+                l++ ;
+            }
         }
-        while(start<n) 
-        nums[start++]=0;
     }
 };
