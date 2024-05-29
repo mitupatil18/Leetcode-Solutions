2b6d1ffa33a8fa3647c9bf19a04c0b9804953d8a
Mitali Patil <mitupatil39@gmail.com>
2024-05-29 18:21:38 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (21.72%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& m) {
-        int n = m.size()-1;
-        vector<vector<int>> vis(n+1, vector<int>(n+1, 0));
+        int n = m.size();
         for(int i = 0 ; i<m.size() ;i++)
         {
-            for(int j = 0 ; j<m[i].size(); j++)
+            for(int j = 0 ; j<=i ;j++)
             {
-                if(!vis[i][j])
-                {
-                    swap(m[i][j],m[j][n-i]);
-                    vis[i][j] = vis[j][n-i] = 1;
-                }
+                swap(m[i][j],m[j][i]);
+                
             }
         }
-        
+        for(int i=0;i<n;i++){
+            reverse(m[i].begin(), m[i].end());
+        }
     }
 };
