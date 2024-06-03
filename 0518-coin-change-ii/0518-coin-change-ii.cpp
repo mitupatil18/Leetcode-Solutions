94e59e2a463991f257939b6891f4d9dddafdf152
Mitali Patil <mitupatil39@gmail.com>
2024-06-03 09:54:58 +0530

    Time: 0 ms (100%), Space: 8.4 MB (95.01%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int change(int sum, vector<int>& coins) {
-        vector<long long> dp(sum + 1,0);
-        int N = coins.size();
+        vector<int> dp(sum + 1,0);
+        int n = coins.size();
         dp[0] = 1;
-        for(int i=0;i<N;i++){
+        for(int i=0;i<n;i++){
             for(int j=0;j<=sum-coins[i];j++){
                 dp[j + coins[i]] += dp[j];
             }
