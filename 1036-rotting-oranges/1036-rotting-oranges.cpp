6fd323bdee4ccac049446f2a2218d16368413110
Mitali Patil <mitupatil39@gmail.com>
2024-06-06 19:19:35 +0530

    Time: 10 ms (19.04%), Space: 15.8 MB (66.15%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,52 +1,52 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& g) {
+        int m = g.size(), n = g[0].size();
+        int ans = -1 , cnt = 0, org = 0  ;
         queue<pair<int,int>> q;
-        int m = g.size() , n = g[0].size();
-        int ans = 0 , cnt = 0 ;
-        for(int i = 0 ; i<g.size() ; i++)
+        for(int i = 0 ; i<m ; i++)
         {
-            for(int j = 0; j<g[0].size() ; j++)
+            for(int j = 0 ; j<n ; j++)
             {
-                if(g[i][j]!=0)
-                cnt++;
+                if(g[i][j]==1 || g[i][j]==2)
+                org++;
                 if(g[i][j]==2)
-                 q.push({i,j});
+                q.push({i,j});
             }
         }
-        int days =  0 ;
+        if(org == 0) return 0;
         while(!q.empty())
         {
-            int size = q.size() ;
-            ans += size ;
+            int size = q.size();
+            cnt += size;
             for(int i = 0 ; i<size; i++)
             {
                 int x = q.front().first;
                 int y = q.front().second;
                 q.pop();
-                if(x-1>=0 && g[x-1][y]==1 )
+                if(x-1>=0 && g[x-1][y]==1)
                 {
-                    q.push({x-1,y});
                     g[x-1][y] = 2;
+                    q.push({x-1,y});
                 }
-                if(y-1>=0 && g[x][y-1]==1 )
+                if(y-1>=0 && g[x][y-1]==1)
                 {
-                    q.push({x,y-1});
                     g[x][y-1] = 2;
-                } 
+                    q.push({x,y-1});
+                }
                 if(x+1<m && g[x+1][y]==1)
                 {
-                    q.push({x+1,y});
                     g[x+1][y] = 2;
+                    q.push({x+1,y});
                 }
-                if(y+1<n && g[x][y+1]==1 )
+                if(y+1<n && g[x][y+1]==1)
                 {
-                    q.push({x,y+1});
                     g[x][y+1] = 2;
+                    q.push({x,y+1});
                 }
             }
-            if(!q.empty() ) days++;
+            ans++;
         }
-        return ans==cnt?days:-1;
+        return org == cnt ? ans : -1;
     }
 };
