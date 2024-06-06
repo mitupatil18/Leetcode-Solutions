c81d3f2a8548cb1bf6cb165ecf577590cc93277b
Mitali Patil <mitupatil39@gmail.com>
2024-06-06 08:34:52 +0530

    Time: 119 ms (5.44%) | Memory: 23.8 MB (96.09%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.cpp
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -1,44 +1,33 @@
 class Solution {
 public:
-    bool isNStraightHand(vector<int>& hand, int size) {
-        map<int,int> m;
-        priority_queue<pair<int,int>> pq;
-        
-        for(int i = 0; i < hand.size(); i++)
-            m[hand[i]]++;
-        
-        for(auto x : m)
-            pq.push({x.first, x.second});
-        
-        while(!pq.empty()) {
-            queue<pair<int,int>> q;
-            vector<int> v;
-            int i = 0;
-            
-            while(!pq.empty() && i < size) {
-                v.push_back(pq.top().first);
-                m[pq.top().first]--;
+    bool isNStraightHand(vector<int>& h, int g) {
+        int n = h.size();
+        if(n%g!=0)
+        return 0;
+        vector<int> vis(n,0);
+        sort(h.begin(),h.end());
+        int cnt = 0 , prev;
+        for(int i = 0 ; i<n/g ; i++)
+        {
+            cnt = 0 , prev = -1;
+            for(int j = 0 ; j<n && cnt<g; j++)
+            {
+                if(vis[j]==0 && cnt==0)
+                {
+                    prev = h[j];
+                    cnt++;
+                    vis[j] = 1;
+                }
+                else if(vis[j]==0 && h[j]-prev==1)
+                {
+                    cnt++;
+                    prev = h[j];
+                    vis[j] = 1;
+                }
                 
-                if(m[pq.top().first] > 0)
-                    q.push({pq.top().first, m[pq.top().first]});
-                
-                pq.pop();
-                i++;
             }
-            
-            if (v.size() != size)
-                return false;
-            
-            for(int i = 0; i < v.size() - 1; i++) {
-                if(abs(v[i+1] - v[i] )!= 1)
-                    return false;
-            }
-            
-            while(!q.empty()) {
-                pq.push(q.front());
-                q.pop();
-            }
+            if(cnt!=g) return 0;
         }
-        return true;
+        return 1;
     }
 };
