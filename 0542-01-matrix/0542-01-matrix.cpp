c6c7ec776203eb3845f4bda28a8898446939f875
Mitali Patil <mitupatil39@gmail.com>
2024-06-07 16:05:32 +0530

    Time: 55 ms (53.87%), Space: 34 MB (73.62%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,36 +1,43 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        queue<pair<int,int>>q;
-        for(int i=0;i<mat.size();i++){
-            for(int j=0;j<mat[0].size();j++){
-                if(mat[i][j]==0)
-                    q.push({i,j});
-                else
-                    mat[i][j]=-1;
+        int m = mat.size(), n = mat[0].size();
+        vector<vector<int>> dist(m, vector<int>(n,INT_MAX));
+        queue<pair<int,int>> q;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 0) {
+                    dist[i][j] = 0;
+                    q.push({i, j});
+                }
             }
         }
-        int length=0;
-        int dirx[]={1,-1,0,0};
-        int diry[]={0,0,1,-1};
-        while(!q.empty()){
-            int size=q.size();
-            length++;
-           while(size--){
-               auto f=q.front();
-               q.pop();
-               int s=f.first;
-               int end=f.second;
-               for(int i=0;i<4;i++){
-                   int r=s+dirx[i];
-                   int c=end+diry[i];
-                   if(r<0 || c<0 || r==mat.size() || c==mat[0].size() || mat[r][c]!=-1)
-                      continue;
-                   mat[r][c]=length;
-                   q.push({r,c});
-               }
-           }
+        while(!q.empty())
+        {
+            int x = q.front().first;
+            int y = q.front().second;
+            q.pop();
+            if(x-1>=0 && mat[x-1][y]==1 && dist[x-1][y] > dist[x][y] + 1)
+            {
+                dist[x-1][y] = dist[x][y]+1;
+                q.push({x-1,y});
+            }
+            if(y-1>=0 && mat[x][y-1]==1 && dist[x][y-1] > dist[x][y] + 1)
+            {
+                dist[x][y-1] = dist[x][y]+1;
+                q.push({x,y-1});
+            }
+            if(x+1<m && mat[x+1][y]==1 && dist[x+1][y] > dist[x][y] + 1)
+            {
+                dist[x+1][y] = dist[x][y]+1;
+                q.push({x+1,y});
+            }
+            if(y+1<n && mat[x][y+1]==1 && dist[x][y+1] > dist[x][y] + 1)
+            {
+                dist[x][y+1] = dist[x][y]+1;
+                q.push({x,y+1});
+            }
         }
-        return mat;
+        return dist;
     }
 };
