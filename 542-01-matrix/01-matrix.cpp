93466679df9090b0c39a133b52498b7441994684
Mitali Patil <mitupatil39@gmail.com>
2024-06-07 16:05:35 +0530

    Time: 55 ms (53.87%) | Memory: 34 MB (73.62%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,39 +1,43 @@
-#include <vector>
-#include <queue>
-#include <climits>
-
-using namespace std;
-
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int n = mat.size(), m = mat[0].size();
-        queue<pair<int, int>> q;
-        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+        int m = mat.size(), n = mat[0].size();
+        vector<vector<int>> dist(m, vector<int>(n,INT_MAX));
+        queue<pair<int,int>> q;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 if (mat[i][j] == 0) {
                     dist[i][j] = 0;
                     q.push({i, j});
                 }
             }
         }
-        int dirx[] = {1, -1, 0, 0};
-        int diry[] = {0, 0, 1, -1};
-        while (!q.empty()) {
-            auto [x, y] = q.front();
+        while(!q.empty())
+        {
+            int x = q.front().first;
+            int y = q.front().second;
             q.pop();
-            
-            for (int i = 0; i < 4; i++) {
-                int r = x + dirx[i];
-                int c = y + diry[i];
-                if (r >= 0 && c >= 0 && r < n && c < m && dist[r][c] > dist[x][y] + 1) {
-                    dist[r][c] = dist[x][y] + 1; 
-                    q.push({r, c}); 
-                }
+            if(x-1>=0 && mat[x-1][y]==1 && dist[x-1][y] > dist[x][y] + 1)
+            {
+                dist[x-1][y] = dist[x][y]+1;
+                q.push({x-1,y});
+            }
+            if(y-1>=0 && mat[x][y-1]==1 && dist[x][y-1] > dist[x][y] + 1)
+            {
+                dist[x][y-1] = dist[x][y]+1;
+                q.push({x,y-1});
+            }
+            if(x+1<m && mat[x+1][y]==1 && dist[x+1][y] > dist[x][y] + 1)
+            {
+                dist[x+1][y] = dist[x][y]+1;
+                q.push({x+1,y});
+            }
+            if(y+1<n && mat[x][y+1]==1 && dist[x][y+1] > dist[x][y] + 1)
+            {
+                dist[x][y+1] = dist[x][y]+1;
+                q.push({x,y+1});
             }
         }
-        
         return dist;
     }
 };
