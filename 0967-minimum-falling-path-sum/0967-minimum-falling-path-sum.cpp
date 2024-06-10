245abc4f197425228f44ba726d8d31a7b9974771
Mitali Patil <mitupatil39@gmail.com>
2024-06-10 12:54:20 +0530

    Time: 6 ms (97.08%), Space: 13.8 MB (10.5%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    int minFallingPathSum(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, 0));
+    int minFallingPathSum(vector<vector<int>>& mat) {
+        int n = mat.size();
+        vector<vector<int>> dp(n,vector<int>(n,0));
         for (int j = 0; j < n; j++) {
-            dp[0][j] = grid[0][j];
+            dp[0][j] = mat[0][j];
         }
-        for (int i = 1; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                int left = (j > 0) ? dp[i-1][j-1] : INT_MAX;
-                int right = (j < n-1) ? dp[i-1][j+1] : INT_MAX;
-                int down = dp[i-1][j];
-                
-                dp[i][j] = grid[i][j] + min({left, down, right});
+        for(int i = 1; i<n ; i++)
+        {
+            for(int j = 0 ; j<n ; j++)
+            {
+                int l = (j>0)?dp[i-1][j-1]:INT_MAX;
+                int r = (j<n-1)?dp[i-1][j+1]:INT_MAX;
+                int d = dp[i-1][j];
+                dp[i][j] = mat[i][j]+ min({l,r,d});
             }
         }
-       int minSum = INT_MAX;
+        int minSum = INT_MAX;
         for (int j = 0; j < n; j++) {
-            minSum = min(minSum, dp[m-1][j]);
+            minSum = min(minSum, dp[n-1][j]);
         }
         
         return minSum;
