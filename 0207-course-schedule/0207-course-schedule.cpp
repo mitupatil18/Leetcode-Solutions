1bc756a4f9067e049e3f2bf11ed4a5c6437f2a11
Mitali Patil <mitupatil39@gmail.com>
2024-06-11 15:30:48 +0530

    Time: 11 ms (92.31%), Space: 17.4 MB (56.38%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,50 +1,34 @@
 class Solution {
 public:
-bool isfound(int node,unordered_map<int,bool> &visited,unordered_map<int,bool> &dfsvisited,unordered_map<int,list<int>> &adj)
-{
-	visited[node]=true;
-	dfsvisited[node]=true;
-	for(auto j:adj[node])
-	{
-		if(!visited[j])
-		{
-			bool checkk=isfound(j,visited,dfsvisited,adj);
-            // cout<<checkk;
-            if (checkk==1) {
-             return true;
-            }
-        }
-		else if(dfsvisited[j])
-		{
-			return true;
-		}
-
-	}
-	dfsvisited[node]=false;
-	return false;
-}
-    bool canFinish(int v, vector<vector<int>>& edges) {
-    unordered_map<int,list<int>> adj;
-    for(int i=0;i<edges.size();i++)
-    {
-        int u=edges[i][0];
-        int v=edges[i][1];
-        adj[u].push_back(v);
-    
-    }
-    unordered_map<int,bool> visited;
-	unordered_map<int,bool> dfsvisited;
-    for(int i=0;i<v;i++)
-    {
-        if(!visited[i])
-        {
-            bool ans=isfound(i,visited,dfsvisited,adj);
-            if (ans) {
-              return false;
-            }
-        }
-    }
-    return true;
-        
+    bool canFinish(int N, vector<vector<int>>& prerequisites) {
+        vector<int> adj[N];
+	    for(auto it : prerequisites)
+	    adj[it[1]].push_back(it[0]);
+	    vector<int> ind(N,0);
+	    for(int i = 0 ; i<N ; i++)
+	    {
+	        for(auto x : adj[i])
+	        ind[x]++ ;
+	    }
+	    queue<int> q;
+	    for(int i = 0 ; i<N ; i++)
+	    {
+	        if(ind[i]==0)
+	        q.push(i);
+	    }
+	    vector<int> topo;
+	    while(!q.empty())
+	    {
+	        int x = q.front();
+	        q.pop();
+	        topo.push_back(x);
+	        for(auto i : adj[x])
+	        {
+	            ind[i]--;
+	            if(ind[i]==0)
+	            q.push(i);
+	        }
+	    }
+	    return topo.size()==N ? 1 :0;
     }
 };
