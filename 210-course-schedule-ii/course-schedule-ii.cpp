a52580dfd5eb2dd77c9ccf6c9cb97d1f5a9b5f37
Mitali Patil <mitupatil39@gmail.com>
2024-06-11 15:33:45 +0530

    Time: 16 ms (56.40%) | Memory: 17.4 MB (50.61%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> findOrder(int N, vector<vector<int>>& prerequisites) {
+        vector<int> adj[N];
+	    for(auto it : prerequisites)
+	    adj[it[1]].push_back(it[0]);
+	    vector<int> ind(N,0);
+	    for(int i = 0 ; i<N ; i++)
+	    {
+	        for(auto x : adj[i])
+	        ind[x]++ ;
+	    }
+	    queue<int> q;
+	    for(int i = 0 ; i<N ; i++)
+	    {
+	        if(ind[i]==0)
+	        q.push(i);
+	    }
+	    vector<int> topo;
+	    while(!q.empty())
+	    {
+	        int x = q.front();
+	        q.pop();
+	        topo.push_back(x);
+	        for(auto i : adj[x])
+	        {
+	            ind[i]--;
+	            if(ind[i]==0)
+	            q.push(i);
+	        }
+	    }
+        if(topo.size()==N)
+	    return topo;
+        return {};
+    }
+};
