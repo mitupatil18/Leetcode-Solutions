5591475e5fb85e5eba5c683c6131a536b2a7c338
Mitali Patil <mitupatil39@gmail.com>
2024-06-13 11:16:26 +0530

    Time: 5 ms (44.90%) | Memory: 11.4 MB (69.71%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -2,20 +2,25 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         stack<int> s;
+        int n = nums2.size();
         map<int,int> m;
-        for(int i = nums2.size()-1 ; i>=0 ; i--)
+        for(int i = nums2.size()-1 ; i>=0 ;i--)
         {
-            while(!s.empty() && nums2[i]>=s.top())
-            s.pop();
-            if(!s.empty())
-            m[nums2[i]] = s.top();
+            while(!s.empty() && s.top()<=nums2[i])
+            {
+                s.pop();
+            }
+            if(s.empty())
+            {
+                m[nums2[i]] = -1;
+            }
             else
-            m[nums2[i]] = -1;
+            m[nums2[i]] = s.top();
             s.push(nums2[i]);
         }
-        for(int i = nums1.size()-1 ; i>=0 ; i--)
+        for(int i = 0 ; i<nums1.size() ; i++)
         {
-            nums1[i] = m[nums1[i]] ;
+            nums1[i] = m[nums1[i]];
         }
         return nums1;
     }
