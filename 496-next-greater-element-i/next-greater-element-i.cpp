c2bd976df2e2cf8304192941f61fe8464d1528d2
Mitali Patil <mitupatil39@gmail.com>
2024-06-13 11:16:53 +0530

    Time: 9 ms (16.86%) | Memory: 11.3 MB (69.71%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -7,21 +7,15 @@ public:
         for(int i = nums2.size()-1 ; i>=0 ;i--)
         {
             while(!s.empty() && s.top()<=nums2[i])
-            {
-                s.pop();
-            }
+            s.pop();
             if(s.empty())
-            {
-                m[nums2[i]] = -1;
-            }
+            m[nums2[i]] = -1;
             else
             m[nums2[i]] = s.top();
             s.push(nums2[i]);
         }
         for(int i = 0 ; i<nums1.size() ; i++)
-        {
-            nums1[i] = m[nums1[i]];
-        }
+        nums1[i] = m[nums1[i]];
         return nums1;
     }
 };
