c423bcb2540b62d2fd9a38809deddecc3bb8d21c
Mitali Patil <mitupatil39@gmail.com>
2024-06-13 09:40:58 +0530

    Time: 0 ms (100%), Space: 8.2 MB (74.6%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -1,37 +1,51 @@
 class MyStack {
 public:
-    queue<int> q1 ;
-    queue<int> q2 ;
+    queue<int> q1;
     MyStack() {
         
     }
     
     void push(int x) {
-         while(!q1.empty()){
-            int temp = q1.front();
-            q1.pop();
-            q2.push(temp);
-        }
         q1.push(x);
-        while(!q2.empty()){
-            int temp = q2.front();
-            q2.pop();
-            q1.push(temp);
-        }
     }
     
     int pop() {
-        int ans = q1.front();
+        queue<int> q2;
+        while(q1.size()>1)
+        {
+            q2.push(q1.front());
+            q1.pop();
+        }
+        int x = q1.front();
         q1.pop();
-        return ans;
+        while(!q2.empty())
+        {
+            q1.push(q2.front());
+            q2.pop();
+        }
+        return x;
     }
     
     int top() {
-        return q1.front();
+        queue<int> q2;
+        while(q1.size()>1)
+        {
+            q2.push(q1.front());
+            q1.pop();
+        }
+        int x = q1.front();
+        q2.push(q1.front());
+        q1.pop();
+        while(!q2.empty())
+        {
+            q1.push(q2.front());
+            q2.pop();
+        }
+        return x;
     }
     
     bool empty() {
-        return q1.empty();
+        return q1.size()==0?1:0;
     }
 };
 
