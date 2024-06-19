5786d24b967e37cee4ac5e052ef8e4f46dd8ed99
Mitali Patil <mitupatil39@gmail.com>
2024-06-19 07:47:16 +0530

    Time: 22 ms (12.01%) | Memory: 15.5 MB (29.57%) - LeetSync

---
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minDistance(string t1, string t2) {
+        int n = t1.size(), m = t2.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        for(int i = 1; i<=n;i++)
+        {
+            for(int j = 1 ; j<=m;j++)
+            {
+                if(t1[i-1]==t2[j-1])
+                dp[i][j] = dp[i-1][j-1]+1;
+                else
+                dp[i][j] = max(dp[i][j-1],dp[i-1][j]);
+            }
+        }
+        return n-dp[n][m]+m-dp[n][m];
+    }
+};
