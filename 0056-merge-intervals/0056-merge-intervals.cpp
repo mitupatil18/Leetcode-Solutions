ec9543cb35ad11ccf58bb27acccd23d7ce940a7e
Mitali Patil <mitupatil39@gmail.com>
2024-06-21 11:50:29 +0530

    Time: 19 ms (85.73%), Space: 22.7 MB (67.95%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>> v;
+        int s = intervals[0][0], e = intervals[0][1], n = intervals.size();
+        for(int i = 1 ; i<n;i++)
+        {
+            if(intervals[i][0]<=e)
+            e = max(e,intervals[i][1]);
+            else
+            {
+                v.push_back({s,e});
+                s = intervals[i][0];
+                e = intervals[i][1];
+            }
+        }
+         v.push_back({s,e});
+        return v;
+    }
+};
