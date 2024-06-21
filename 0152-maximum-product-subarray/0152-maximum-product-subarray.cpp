4237e958c652422233a0e9dceb01c3f5a2974d8e
Mitali Patil <mitupatil39@gmail.com>
2024-06-21 14:22:38 +0530

    Time: 6 ms (43.92%), Space: 16.2 MB (48.61%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,29 +1,22 @@
-#include <vector>
-#include <algorithm>
-
 class Solution {
 public:
-    int maxProduct(std::vector<int>& nums) {
-        long long prod = 1;
-        int n = nums.size();
-        int ans = nums[0];
-        
-        for (int i = 0; i < n; i++) {
+    int  maxProduct(vector<int>& nums) {
+        long long prod = 1 , ans = nums[0];
+        for(long long i = 0 ; i<nums.size();i++)
+        {
             prod *= nums[i];
-            ans = std::max(ans, static_cast<int>(prod));
-            if (prod == 0 || prod<=INT_MIN)
-                prod = 1;
+            ans = max(ans,prod);
+            if(prod==0 || prod<=INT_MIN)
+            prod = 1;
         }
-        
         prod = 1;
-        
-        for (int i = n - 1; i >= 0; i--) {
+        for(long long i = nums.size()-1 ; i>=0;i--)
+        {
             prod *= nums[i];
-            ans = std::max(ans, static_cast<int>(prod));
-            if (prod == 0 || prod<=INT_MIN)
-                prod = 1;
+            ans = max(ans,prod);
+            if(prod==0 || prod<=INT_MIN)
+            prod = 1;
         }
-        
         return ans;
     }
 };
