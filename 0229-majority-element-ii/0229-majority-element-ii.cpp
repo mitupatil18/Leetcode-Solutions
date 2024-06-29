663132794ba25ab1d3b2d6d428c976ca4cd2377c
Mitali Patil <mitupatil39@gmail.com>
2024-06-29 07:27:03 +0530

    Time: 3 ms (98.11%), Space: 18.3 MB (70.77%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int cnt1 = 0 ,cnt2 = 0 , m1 = 0 , m2 = 0 ;
+        int maj1 = 0 , maj2 = 0 , cnt1 = 0 , cnt2 = 0 ;
         for(int i = 0 ; i<nums.size();i++)
         {
-            if(nums[i]==m1)
+            if(nums[i]==maj1)
             cnt1++;
-            else if(nums[i]==m2)
+            else if(nums[i]==maj2)
             cnt2++;
             else if(cnt1==0)
             {
-                m1 = nums[i];
+                maj1 = nums[i];
                 cnt1++;
             }
             else if(cnt2==0)
             {
-                m2 = nums[i];
+                maj2 = nums[i];
                 cnt2++;
             }
             else
@@ -25,19 +25,19 @@ public:
             }
         }
         cnt1 = 0 , cnt2 = 0 ;
-        for(int i  = 0 ;i<nums.size();i++)
+        for(int i = 0 ; i<nums.size();i++)
         {
-            if(nums[i]==m1)
+            if(nums[i]==maj1)
             cnt1++;
-            else if(nums[i]==m2)
+            else if(nums[i]==maj2)
             cnt2++;
         }
         int n = nums.size();
         vector<int> v;
         if(cnt1>n/3)
-        v.push_back(m1);
+        v.push_back(maj1);
         if(cnt2>n/3)
-        v.push_back(m2);
+        v.push_back(maj2);
         return v;
     }
 };
