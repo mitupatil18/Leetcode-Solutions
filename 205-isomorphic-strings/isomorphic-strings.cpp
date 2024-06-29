e7c9e462ba3d6ce39ebe6fb3fc3b786a78e0e68c
Mitali Patil <mitupatil39@gmail.com>
2024-06-29 07:46:48 +0530

    Time: 10 ms (18.89%) | Memory: 9.2 MB (67.72%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,19 +1,28 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        map<char,char> m1 ;
-        map<char,char> m2;
-        if(s.size()!=t.size())
-        return 0;
+        if(s.size()!=t.size()) return 0;
+        map<char,char> m1 , m2;
         for(int i = 0 ; i<s.size();i++)
         {
-            m1[s[i]] = t[i];
-            m2[t[i]] = s[i];
-        }
-        for(int i = 0 ; i<s.size();i++)
-        {
-            if(m1[s[i]]!=t[i] || m2[t[i]]!=s[i])
-            return 0;
+            if(m1.find(s[i])!=m1.end())
+            {
+                if(m1[s[i]]!=t[i])
+                return 0;
+            }
+            else if(m1.find(s[i])==m1.end())
+            {
+                m1[s[i]] = t[i];
+            }
+            if(m2.find(t[i])!=m2.end())
+            {
+                if(m2[t[i]]!=s[i])
+                return 0;
+            }
+            else if(m2.find(t[i])==m2.end())
+            {
+                m2[t[i]] = s[i];
+            }
         }
         return 1;
     }
