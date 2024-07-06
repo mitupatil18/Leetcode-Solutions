c01f1e45f29cb0559d8540736282fb8ca9873316
Mitali Patil <mitupatil39@gmail.com>
2024-07-06 07:18:27 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (7.27%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        //Dutch national flag problem (red, white, blue)
-        const char red=0, white=1, blue=2;
-        int l=0, m=0, r=nums.size()-1;// pointers to partition
-        while(m<=r){
-            switch(nums[m]){
-                case red:
-                    swap(nums[l], nums[m]);
-                    l++, m++;
-                    break;
-                case white:
-                    m++;
-                    break;
-                case blue:
-                    swap(nums[m], nums[r]);
-                    r--;
+        int l = 0, m = 0 , r = nums.size()-1;
+        while(m<=r)
+        {
+            switch(nums[m])
+            {
+                case 0 : swap(nums[l],nums[m]);
+                l++;
+                m++ ;
+                break;
+                case 1 : m++ ;
+                break;
+                case 2 : swap(nums[r],nums[m]);
+                r--;
+                break;
             }
         }
     }
