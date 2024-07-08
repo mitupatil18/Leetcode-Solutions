d4007e6fa7b1166edfd7672282caf6ebd4a72861
Mitali Patil <mitupatil39@gmail.com>
2024-07-08 17:30:19 +0530

    Time: 259 ms (5.07%), Space: 95.9 MB (9.33%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        for(int i = 0 ; i<nums.size();i++)
-        {
-            if(nums[i]%2==0)
-            nums[i] = 0;
-            else
-            nums[i] = 1;
-        }
-        int sum = 0 , ans = 0 ;
+        for(int i = 0 ;i<nums.size();i++)
+        nums[i] = nums[i]%2;
         map<int,int> m;
-        //m[sum] = 0;
+        int sum = 0 , ans = 0 ;
+        m[0] = 1;
         for(int i = 0 ; i<nums.size();i++)
         {
             sum += nums[i];
-            if(sum==k)
-            ans++;
             if(m.find(sum-k)!=m.end())
             ans += m[sum-k];
             m[sum]++;
