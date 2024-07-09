25d9cafc2580433cfe2f667754899016b9921a72
Mitali Patil <mitupatil39@gmail.com>
2024-07-09 08:23:30 +0530

    Time: 148 ms (64.12%), Space: 76.4 MB (95.59%) - LeetHub

--- 1803-average-waiting-time/1803-average-waiting-time.cpp
+++ 1803-average-waiting-time/1803-average-waiting-time.cpp
@@ -2,11 +2,13 @@ class Solution {
 public:
     double averageWaitingTime(vector<vector<int>>& c) {
         double ans = 0.0f;
-        int curr = 0;
+        int curr = c[0][0] ;
         for(int i = 0 ; i<c.size();i++)
         {
-            curr  = max(curr,c[i][0])+c[i][1];
-            ans += curr-c[i][0];
+            if(curr<c[i][0])
+            curr = c[i][0];
+            curr += c[i][1];
+            ans += (curr-c[i][0]);
         }
         return ans/c.size() ;
     }
