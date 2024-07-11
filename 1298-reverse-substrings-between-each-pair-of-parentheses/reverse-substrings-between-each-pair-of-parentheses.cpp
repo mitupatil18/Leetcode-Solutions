a89703085bebdc33c03e529443d2620a845b48e2
Mitali Patil <mitupatil39@gmail.com>
2024-07-11 08:07:26 +0530

    Time: 4 ms (27.22%) | Memory: 8.1 MB (41.81%) - LeetSync

---
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string reverseParentheses(string s) {
+        stack<string> stk;
+        string current;
+        
+        for (char c : s) {
+            if (c == '(') {
+                stk.push(current);
+                current = "";
+            } else if (c == ')') {
+                reverse(current.begin(), current.end());
+                current = stk.top() + current;
+                stk.pop();
+            } else {
+                current += c;
+            }
+        }
+        
+        return current;
+    }
+};
