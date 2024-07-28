b72707e81c1bfd37c5d9303aaffe3c2320571a24
Mitali Patil <mitupatil39@gmail.com>
2024-07-28 09:05:48 +0530

    Time: 403 ms (60.29%), Space: 0B (100%) - LeetHub

--- 1258-article-views-i/1258-article-views-i.sql
+++ 1258-article-views-i/1258-article-views-i.sql
@@ -1,2 +1,8 @@
 # Write your MySQL query statement below
-select distinct author_id as id from Views where author_id = viewer_id order by id;
+select distinct(v1.author_id) as id from views  as v1
+inner join 
+views v2 
+on v1.author_id=v2.author_id
+where 
+v1.author_id=v2.viewer_id
+order by id;
