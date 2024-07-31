91ffc01e56bf83e32dd76d237c2607201a0128b4
Mitali Patil <mitupatil39@gmail.com>
2024-07-31 15:21:32 +0530

    Time: 13 ms (9.34%), Space: 18.1 MB (41.5%) - LeetHub

--- 1651-shuffle-string/1651-shuffle-string.cpp
+++ 1651-shuffle-string/1651-shuffle-string.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     string restoreString(string s, vector<int>& indices) {
-        map<int,char> m;
+        string a = s;
         for(int i = 0 ; i<s.size();i++)
         {
-            m[indices[i]] = s[i];
+            a[indices[i]] = s[i];    
         }
-        int i = 0 ;
-        for(auto x : m)
-        {
-            s[i] = x.second;
-            i++;
-        }
-        return s;
+        return a;
     }
 };
