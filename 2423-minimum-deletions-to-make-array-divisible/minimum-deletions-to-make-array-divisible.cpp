4f805ba11c326043f6a7bfecca5febf59dce9371
Mitali Patil <mitupatil39@gmail.com>
2024-08-17 12:28:19 +0530

    Time: 106 ms (72.68%) | Memory: 92 MB (26.29%) - LeetSync

---
+++ 2423-minimum-deletions-to-make-array-divisible/minimum-deletions-to-make-array-divisible.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, vector<int>& numsDivide) {
+        sort(nums.begin(),nums.end());
+        if(nums[0]==1)return 0;
+        int g = 0;
+        for(auto &i: numsDivide){
+            g = __gcd(g,i);
+        }
+        if(g==1||g<nums[0]){
+            return -1;
+        }
+        int i;
+        for(i = 0;i<nums.size();i++){
+            if(g%nums[i] == 0){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
