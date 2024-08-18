3e595208622ad82157fcb10a1869661cbea122b1
Mitali Patil <mitupatil39@gmail.com>
2024-08-18 08:48:47 +0530

    Time: 215 ms (63.64%), Space: 210.8 MB (54.55%) - LeetHub

--- 3525-maximum-energy-boost-from-two-drinks/3525-maximum-energy-boost-from-two-drinks.cpp
+++ 3525-maximum-energy-boost-from-two-drinks/3525-maximum-energy-boost-from-two-drinks.cpp
@@ -4,8 +4,8 @@ public:
         int n = a.size();
         vector<long long> x(n);
         vector<long long> y(n);
-        x[0] = a[0];
         y[0] = b[0];
+        x[0] = a[0];
         for (int i = 1; i < n; ++i) {
             y[i] = max(y[i-1] + b[i], x[i-1]);
             x[i] = max(x[i-1] + a[i], y[i-1]);
