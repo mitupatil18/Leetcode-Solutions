7707a6454ead08ba8fc9fdcdfd1d435a7f259d02
Mitali Patil <mitupatil39@gmail.com>
2024-12-03 08:35:15 +0530

    Time: 7 ms (99.10%) | Memory: 82.6 MB (86.84%) - LeetSync

---
+++ 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string addSpaces(string s, vector<int>& sp) {
+        int n = sp.size();
+        int i = s.size()-1;
+        for(int j = 0 ; j<n ;j++)
+        s += " ";
+        int k = s.size()-1;
+        int j = sp.size()-1;
+        cout<<i<<" "<<j<<" "<<k<<endl;
+        while(i>=0 && j>=0)
+        {
+            if(i!=sp[j])
+            {
+                swap(s[i],s[k]);
+                i--;
+                k--;
+            }
+            else
+            {
+                swap(s[i],s[k]);
+                k--;
+                j--;
+            }
+        }
+        return s;
+    }
+};
