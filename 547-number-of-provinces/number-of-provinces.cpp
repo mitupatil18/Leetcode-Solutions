7510087533ad3e5fc9b326ec40809c6a89309b91
Mitali Patil <mitupatil39@gmail.com>
2024-12-14 12:11:11 +0530

    Time: 4 ms (34.44%) | Memory: 20.3 MB (23.03%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
-    int findCircleNum(vector<vector<int>>& conn) {
-        int n = conn.size();
-        vector<int> vis(n,0);
-        int ans = 0 ;
-        for(int i = 0 ; i<n ; i++)
+    void dfs(int i , vector<vector<int>>& adj, vector<int>& vis)
+    {
+        vis[i] = 1;
+        for(auto x : adj[i])
         {
-            if(vis[i]==1)
-            continue;
-            ans++;
-            queue<int> q;
-            q.push(i);
-            while(!q.empty())
+            if(!vis[x])
+            dfs(x,adj,vis);
+        }
+    }
+    int findCircleNum(vector<vector<int>>& g) {
+        int n = g.size(), ans = 0;
+        vector<int> vis(n,0);
+        vector<vector<int>> adj(n);
+        for(int i = 0 ; i<g.size(); i++)
+        {
+            for(int j = 0 ; j<g[0].size();j++)
             {
-                int x = q.front();
-                q.pop();
-                vis[x] = 1;
-                for(int j = 0 ; j<n ; j++)
-                {
-                    if(conn[x][j]==1 && !vis[j])
-                    {
-                        q.push(j);
-                    }
-                }
+                if(g[i][j]==1 && i!=j)
+                adj[i].push_back(j);
+            }
+        }
+        for(int i = 0; i<n ;i++)
+        {
+            if(!vis[i])
+            {
+                dfs(i,adj,vis);
+                ans++;
             }
         }
         return ans;
