bdba142a2a6c041c511eb5768624de250446c809
Mitali Patil <mitupatil39@gmail.com>
2024-12-14 13:57:40 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (17.12%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,61 +1,47 @@
 class Solution {
 public:
-    void solve(vector<vector<char>>& g) {
-        int n = g.size(), m = g[0].size();
-        vector<vector<int>> v(n, vector<int>(m, 0));
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                int z = 0;
-                if (!v[i][j] && g[i][j] == 'O') {
-                    vector<pair<int, int>> vp;
-                    v[i][j] = 1;
-                    queue<pair<int, int>> q;
-                    q.push({i, j});
-                    if (i == 0 || i == n - 1 || j == 0 || j == m - 1)
-                        z = 1;
-                    vp.push_back({i, j});
-                    while (!q.empty()) {
-                        int x = q.front().first, y = q.front().second;
-                        q.pop();
-                        if (x + 1 < n && !v[x + 1][y] && g[x + 1][y] == 'O') {
-                            v[x + 1][y] = 1;
-                            q.push({x + 1, y});
-                            if (x+1 == n - 1 || y == m - 1 || x+1==0 || y==0)
-                                z = 1;
-                            vp.push_back({x+1, y});
-                        }
-                        if (x - 1 >= 0 && !v[x - 1][y] && g[x - 1][y] == 'O') {
-                            v[x - 1][y] = 1;
-                            q.push({x - 1, y});
-                            if (x-1 == n - 1 || y == m - 1 || x-1==0 || y==0)
-                                z = 1;
-                            vp.push_back({x-1, y});
-                        }
-                        if (y - 1 >= 0 && !v[x][y - 1] && g[x][y - 1] == 'O') {
-                            v[x][y - 1] = 1;
-                            q.push({x, y - 1});
-                            if (x == n - 1 || y-1 == m - 1 || x==0 || y-1==0)
-                                z = 1;
-                            vp.push_back({x, y-1});
-                        }
-                        if (y + 1 < m && !v[x][y + 1] && g[x][y + 1] == 'O') {
-                            v[x][y + 1] = 1;
-                            q.push({x, y + 1});
-                            if (x == n - 1 || y+1 == m - 1 || x==0 || y+1==0)
-                                z = 1;
-                            vp.push_back({x, y+1});
-                        }
-                    }
-                    if(z!=1)
-                    {
-                        for(auto x : vp)
-                        {
-                            g[x.first][x.second] = 'X' ;
-                        }
-                    }
+    void solve(vector<vector<char>>& b) {
+        int m = b.size(), n= b[0].size();
+        queue<pair<int,int>> q;
+        for(int i = 0 ; i<n ;i++)
+        {
+            if(b[0][i]=='O')
+            q.push({0,i});
+            if(b[m-1][i]=='O')
+            q.push({m-1,i});
+        }
+        for(int i = 0 ; i<m ;i++)
+        {
+            if(b[i][0]=='O')
+            q.push({i,0});
+            if(b[i][n-1]=='O')
+            q.push({i,n-1});
+        }
+        int dx[] = {-1,1,0,0}, dy[] = {0,0,-1,1};
+        while(!q.empty())
+        {
+            auto f = q.front();
+            b[f.first][f.second] = 'K';
+            q.pop();
+            for(int i = 0 ; i<4 ;i++)
+            {
+                int nx = f.first+dx[i], ny = f.second+dy[i];
+                if(nx>=0 && nx<m && ny>=0 && ny<n && b[nx][ny]=='O')
+                {
+                    q.push({nx,ny});
                 }
             }
         }
+        for(int i = 0 ; i<m ;i++)
+        {
+            for(int j = 0 ; j<n ;j++)
+            {
+                if(b[i][j]=='K')
+                b[i][j] = 'O';
+                else
+                b[i][j] = 'X';
+            }
+        }
+        
     }
 };
