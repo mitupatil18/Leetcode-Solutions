4ab7089217e236cefa7336274ec812d76c779d6a
Mitali Patil <mitupatil39@gmail.com>
2024-12-14 15:37:25 +0530

    Time: 67 ms (65.95%) | Memory: 19.5 MB (81.77%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,32 +1,40 @@
 class Solution {
 public:
-    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        queue<pair<string,int>> q;
-        q.push({beginWord,1});
+    int ladderLength(string beginWord, string endWord,
+                     vector<string>& wordList) {
         unordered_set<string> s(wordList.begin(), wordList.end());
-        s.erase(beginWord);
-        while(!q.empty())
-        {
-            string word = q.front().first;
-            int steps = q.front().second;
-            q.pop();
-            if(word==endWord)
-            return steps;
-            for(int i = 0 ; i<word.size();i++)
-            {
-                char org = word[i];
-                for(char ch  = 'a' ; ch<='z' ; ch++)
-                {
-                    word[i] = ch;
-                    if(s.find(word)!=s.end())
-                    {
-                        s.erase(word);
-                        q.push({word,steps+1});
+        if (s.find(endWord) == s.end())
+            return 0;
+        queue<string> q;
+        q.push(beginWord);
+        int ladderLength = 1;
+        while (!q.empty()) {
+            int levelSize = q.size();
+            for (int i = 0; i < levelSize; i++) {
+                string currentWord = q.front();
+                q.pop();
+
+                for (int j = 0; j < currentWord.size(); j++) {
+                    string transformedWord = currentWord;
+
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        transformedWord[j] = c;
+
+                        if (transformedWord == endWord) {
+                            return ladderLength + 1;
+                        }
+
+                        if (s.find(transformedWord) != s.end()) {
+                            q.push(transformedWord);
+                            s.erase(transformedWord);
+                        }
                     }
                 }
-                word[i] = org;
             }
+
+            ladderLength++;
         }
+
         return 0;
     }
 };
