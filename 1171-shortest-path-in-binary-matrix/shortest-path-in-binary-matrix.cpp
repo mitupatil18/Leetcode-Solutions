a56e2a6bad0919b2905cb1495ef8fc255baf6d62
Mitali Patil <mitupatil39@gmail.com>
2024-12-17 10:21:46 +0530

    Time: 20 ms (57.55%) | Memory: 25.2 MB (49.87%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,35 +1,38 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& g) {
+        queue<pair<int,int>> q;
+        int ans = 1 ;
         int n = g.size();
-        if (g[0][0] == 1 || g[n-1][n-1] == 1) return -1;
-        if (n == 1) return 1;
-        queue<pair<int,pair<int,int>>> q;
-        vector<vector<int>> dist(n, vector<int>(n, 1e9));
-        dist[0][0] = 1;
-        q.push({1, {0, 0}});
-        int dr[] = {-1, -1, -1, 0, 0, 1, 1, 1};
-        int dc[] = {-1, 0, 1, -1, 1, -1, 0, 1};
-
-        while (!q.empty()) {
-            auto it = q.front();
-            q.pop();
-            int dis = it.first;
-            int r = it.second.first;
-            int c = it.second.second;
-
-            for (int i = 0; i < 8; i++) {
-                int nr = r + dr[i];
-                int nc = c + dc[i];
-                if (nr >= 0 && nr < n && nc >= 0 && nc < n && g[nr][nc] == 0 && dis + 1 < dist[nr][nc]) {
-                    dist[nr][nc] = dis + 1;
-                    if (nr == n - 1 && nc == n - 1)
-                        return dis + 1;
-                    q.push({dis + 1, {nr, nc}});
+        if(g[0][0]==1 || g[n-1][n-1]) return -1;
+        vector<vector<int>> vis(n,vector<int>(n,0));
+        q.push({0,0});
+        vis[0][0] = 1;
+        int dx[] = {-1,-1,-1,0,1,1,1,0};
+        int dy[] = {-1,0,1,1,1,0,-1,-1};
+        while(!q.empty())
+        {
+            int size = q.size();
+            for(int i = 0; i<size ;i++)
+            {
+                auto f = q.front();
+                int x = f.first, y = f.second;
+                if(x==n-1 && y==n-1)return ans;
+                vis[x][y] = 1;
+                q.pop();
+                for(int j = 0 ; j<8 ;j++)
+                {
+                    int nx = x+dx[j];
+                    int ny = y+dy[j];
+                    if(nx>=0 && ny>=0 && nx<n && ny<n && g[nx][ny]==0 && !vis[nx][ny])
+                    {
+                        vis[nx][ny] = 1;
+                        q.push({nx,ny});
+                    }
                 }
             }
+            ans++;
         }
-
         return -1;
     }
 };
