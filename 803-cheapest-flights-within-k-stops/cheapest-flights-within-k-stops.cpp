5e374967ebed6faaed84c764bce264d8bbaf1da6
Mitali Patil <mitupatil39@gmail.com>
2024-12-18 11:38:48 +0530

    Time: 3 ms (65.15%) | Memory: 19.1 MB (15.09%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,18 +1,42 @@
 class Solution {
 public:
-    //bellman ford.
-    //just run it k+1 iterations.
-    int findCheapestPrice(int n, vector<vector<int>>& a, int src, int sink, int k) {
-        
-        vector<int> c(n, 1e8);
-        c[src] = 0;
-        
-        for(int z=0; z<=k; z++){
-            vector<int> C(c);
-            for(auto e: a)
-                C[e[1]] = min(C[e[1]], c[e[0]] + e[2]);
-            c = C;
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        // Adjacency list to store the graph
+        vector<vector<pair<int, int>>> adj(n);
+        for (auto& flight : flights) {
+            adj[flight[0]].emplace_back(flight[1], flight[2]);
         }
-        return c[sink] == 1e8 ? -1 : c[sink];
+
+        // Priority queue to store {cost, current node, stops used}
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>> pq;
+        pq.emplace(0, src, 0);
+
+        // To store the minimum cost to reach a node with at most 'k' stops
+        vector<vector<int>> minCost(n, vector<int>(k + 2, INT_MAX));
+        minCost[src][0] = 0;
+
+        while (!pq.empty()) {
+            auto [cost, node, stops] = pq.top();
+            pq.pop();
+
+            // If destination is reached, return the cost
+            if (node == dst) return cost;
+
+            // If stops exceed the allowed limit, skip
+            if (stops > k) continue;
+
+            // Explore neighbors
+            for (auto& [next, price] : adj[node]) {
+                int newCost = cost + price;
+                // Only add to queue if this path is cheaper
+                if (newCost < minCost[next][stops + 1]) {
+                    minCost[next][stops + 1] = newCost;
+                    pq.emplace(newCost, next, stops + 1);
+                }
+            }
+        }
+
+        // If no path is found, return -1
+        return -1;
     }
 };
