cb28fb7be98f7dd0d26711eb3922c72bebb22c80
Mitali Patil <mitupatil39@gmail.com>
2024-12-25 10:27:46 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (21.71%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    int find(int i, vector<int>& dp, int n, vector<int>& nums)
+    int find(int i , int n, vector<int>& nums,vector<int>& dp)
     {
         if(i>=n)
         return 0;
-        if(dp[i]!=-1)
-        return dp[i];
+        if(dp[i]!=-1)return dp[i];
         if(i==n-1)
         return dp[i] = nums[i];
-        dp[i] = max(find(i+1,dp,n, nums), find(i+2,dp,n, nums)+nums[i]);
+        int take = find(i+1,n,nums,dp);
+        int nottake = find(i+2,n,nums,dp)+nums[i];
+        dp[i] = max(take,nottake);
         return dp[i];
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n+1,-1);
-        return find(0,dp,n, nums);
+        return find(0,n,nums,dp);
     }
 };
