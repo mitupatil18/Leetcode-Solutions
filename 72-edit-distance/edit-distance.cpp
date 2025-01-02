3c6b7ff4ad985e06e0f470a6167dc446e3c6df48
Mitali Patil <mitupatil39@gmail.com>
2025-01-02 10:38:41 +0530

    Time: 4 ms (77.81%) | Memory: 10.8 MB (87.31%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -12,7 +12,7 @@ public:
                 else if(s[i-1]==t[j-1])
                 dp[i][j] = dp[i-1][j-1];
                 else
-                dp[i][j] = 1+min(dp[i][j-1], min(dp[i-1][j],dp[i-1][j-1]));
+                dp[i][j] = 1+min({dp[i][j-1],dp[i-1][j],dp[i-1][j-1]});
             }
         }
         return dp[n][m];
