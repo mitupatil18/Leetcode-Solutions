0f2cdd2b790c55589999f59236a3c4a11eefc52d
Mitali Patil <mitupatil39@gmail.com>
2025-01-07 08:49:18 +0530

    Time: 4 ms (31.92%) | Memory: 11.3 MB (60.52%) - LeetSync

---
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<string> stringMatching(vector<string>& w) {
+        vector<string> v;
+        for(int i = 0 ; i<w.size();i++)
+        {
+            string s = w[i];
+            for(int j = 0 ; j<w.size();j++)
+            {
+                if(i==j)continue;
+                if (w[j].find(w[i]) != string::npos) 
+                {
+                    v.push_back(w[i]);
+                    break;
+                }
+            }
+        }
+        return v;
+    }
+};
